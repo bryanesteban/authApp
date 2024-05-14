388ea3494ae47621b4d115e0d3c627f13f349dee
bryanesteban <bryanesteban_123@hotmail.com>
2024-05-14 00:07:41 -0500

    conexion with backend in nest

--- src/app/auth/pages/login-page/login-page.component.ts
+++ src/app/auth/pages/login-page/login-page.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-login-page',
@@ -7,4 +9,28 @@ import { Component } from '@angular/core';
 })
 export class LoginPageComponent {
 
+
+
+   private fb = inject( FormBuilder);
+   private authService = inject( AuthService);
+  constructor() {}
+  // constructor(private fb:FormBuilder) {}
+
+  public myForm: FormGroup = this.fb.group({
+    email:['', [Validators.required, Validators.email]],
+    password:['', [Validators.required, Validators.minLength(6)]],
+  });
+
+
+  login (){
+    const{ email, password} = this.myForm.value;
+
+    this.authService.login(email, password)
+    .subscribe( success => {
+
+      console.log({success});
+    })
+
+
+  }
 }
